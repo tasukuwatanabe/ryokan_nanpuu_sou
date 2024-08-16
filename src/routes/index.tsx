faea9d94246263b6bf15a3f46413bc64b72baecb
tasukuwatanabe <tasuku.watanabe.325@gmail.com>
2024-08-16 21:53:09 +0900

    refactor: 部屋データを外部ファイルに移動

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,69 +2,11 @@ import React, { useEffect, useState } from "react";
 import { createFileRoute } from "@tanstack/react-router";
 
 import { type Room } from "../types";
+import { roomList } from "../consts";
 import PageGrid from "../components/PageGrid";
 import RoomSearch from "../components/RoomSearch";
 import RoomIndex from "../components/RoomIndex";
 import { parseDateStringToMidnight } from "../utils";
-
-const roomList = [
-  {
-    id: 1,
-    name: "梅の間",
-    price: 9800,
-    description: "海が一望できる",
-    image: "example_1.jpg",
-    wifi: false,
-    smoking: true,
-    breakfast: false,
-    reservedDates: [
-      new Date(2024, 7, 20),
-      new Date(2024, 7, 21),
-      new Date(2024, 7, 22),
-    ],
-  },
-  {
-    id: 2,
-    name: "竹の間",
-    price: 15800,
-    description: "山が一望できる",
-    image: "example_2.jpg",
-    wifi: true,
-    smoking: false,
-    breakfast: false,
-    reservedDates: [
-      new Date(2024, 7, 25),
-      new Date(2024, 7, 26),
-      new Date(2024, 7, 27),
-    ],
-  },
-  {
-    id: 3,
-    name: "松の間",
-    price: 19800,
-    description: "夕焼けが楽しめる",
-    image: "example_3.jpg",
-    wifi: true,
-    smoking: false,
-    breakfast: true,
-    reservedDates: [new Date(2024, 7, 1)],
-  },
-  {
-    id: 4,
-    name: "鶴の間",
-    price: 29800,
-    description: "海が一望できる",
-    image: "example_4.jpg",
-    wifi: true,
-    smoking: false,
-    breakfast: false,
-    reservedDates: [
-      new Date(2024, 7, 29),
-      new Date(2024, 7, 30),
-      new Date(2024, 7, 31),
-    ],
-  },
-];
 
 const Index = () => {
   const today = new Date();
