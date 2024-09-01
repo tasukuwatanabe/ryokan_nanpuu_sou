212888e6bce5599875e1edec7c295ef4ebed1680
tasukuwatanabe <tasuku.watanabe.325@gmail.com>
2024-09-01 19:24:23 +0900

    feat: Headerをshadcnでマイグレーション

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,78 +1,60 @@
-import styled from "styled-components";
 import { Link } from "@tanstack/react-router";
+import { CircleUser } from "lucide-react";
 
-import { useAuth } from "../contexts/authContext";
-import Container from "./Container";
 import { doSignOut } from "../firebase/auth";
-
-const HEADER_Header = styled.header`
-  box-shadow: 0 0 2px rgba(0, 0, 0, 0.3);
-`;
-
-const DIV_HeaderInner = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-const H1_Title = styled.h1`
-  font-size: 20px;
-`;
-
-const UL_NavList = styled.ul`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  column-gap: 50px;
-  list-style-type: none;
-`;
-
-const LINK_Logo = styled(Link)`
-  text-decoration: none;
-  color: #333;
-  font-size: 22px;
-  font-weight: 700;
-`;
-
-const LINK_NavLink = styled(Link)`
-  text-decoration: none;
-  color: #333;
-  font-size: 16px;
-`;
-
-const SPAN_SignOut = styled.span`
-  text-decoration: underline;
-  font-size: 13px;
-  cursor: pointer;
-`;
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { useAuth } from "../contexts/authContext";
+import { Button } from "./ui/button";
 
 const Header = () => {
   const { currentUser } = useAuth();
 
   return (
-    <HEADER_Header>
-      <Container>
-        <DIV_HeaderInner>
-          <H1_Title>
-            <LINK_Logo to="/">南風荘</LINK_Logo>
-          </H1_Title>
-          <nav>
-            <UL_NavList>
-              <li>
-                {currentUser?.email ? (
-                  <p>
-                    {currentUser.email} [
-                    <SPAN_SignOut onClick={doSignOut}>ログアウト</SPAN_SignOut>]
-                  </p>
-                ) : (
-                  <LINK_NavLink to="/login">ログイン</LINK_NavLink>
-                )}
-              </li>
-            </UL_NavList>
-          </nav>
-        </DIV_HeaderInner>
-      </Container>
-    </HEADER_Header>
+    <header className="sticky top-0 border-b bg-background">
+      <div className="max-w-[1000px] flex h-16 justify-between items-center m-auto px-6">
+        <nav className="flex-col gap-6 text-lg font-medium">
+          <Link to="/" className="text-xl font-semibold">
+            南風荘
+          </Link>
+        </nav>
+        <div className="flex items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
+          {currentUser?.email ? (
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button
+                  variant="secondary"
+                  size="icon"
+                  className="rounded-full hover:cursor-pointer outline-none focus-visible:ring-0 focus-visible:ring-offset-0"
+                >
+                  <CircleUser className="h-5 w-5" />
+                  <span className="sr-only">Toggle user menu</span>
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuItem className="hover:cursor-pointer">
+                  マイページ
+                </DropdownMenuItem>
+                <DropdownMenuItem
+                  onClick={doSignOut}
+                  className="hover:cursor-pointer"
+                >
+                  ログアウト
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          ) : (
+            <Link to="/login" className="">
+              ログイン
+            </Link>
+          )}
+        </div>
+      </div>
+    </header>
   );
 };
 
