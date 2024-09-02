a4302b78c0a04d2e048ccbbda3940f496bde7565
tasukuwatanabe <tasuku.watanabe.325@gmail.com>
2024-09-02 23:52:59 +0900

    fix: RoomSortをTailwind CSSで書き換え

--- src/components/RoomSort.tsx
+++ src/components/RoomSort.tsx
@@ -1,39 +1,7 @@
 import { Dispatch, SetStateAction } from "react";
 import { ReactNode } from "@tanstack/react-router";
-import styled from "styled-components";
 
 import { SortType } from "@/types";
-
-const UL_SortList = styled.ul`
-  display: flex;
-  justify-content: flex-end;
-  list-style-type: none;
-  @media screen and (max-width: 767px) {
-    margin-bottom: 15px;
-  }
-  @media screen and (min-width: 768px) {
-    position: absolute;
-    top: -30px;
-    right: 0;
-  }
-`;
-
-const LI_SortItem = styled.li`
-  font-size: 14px;
-  line-height: 1.1;
-  padding-inline: 10px;
-  border-left: 1px solid #d0d0d0;
-
-  &:last-of-type {
-    border-right: 1px solid #d0d0d0;
-  }
-
-  &:not(.active) {
-    color: #ad9b3c;
-    cursor: pointer;
-    text-decoration: underline;
-  }
-`;
 
 interface RoomSortT {
   sortType: SortType;
@@ -49,20 +17,22 @@ const SortList = ({ sortType, setSortType }: RoomSortT) => {
     children: ReactNode;
   }) => {
     return (
-      <LI_SortItem
-        className={sortType === sort ? "active" : ""}
-        onClick={() => setSortType(sort)}
+      <li
+        className={`${sortType !== sort ? "text-sky-500 underline cursor-pointer" : ""} text-sm px-3 border-r-2 leading-4`}
+        onClick={() => {
+          if (sortType !== sort) setSortType(sort);
+        }}
       >
         {children}
-      </LI_SortItem>
+      </li>
     );
   };
 
   return (
-    <UL_SortList>
+    <ul className="flex justify-end text-sm border-l-2 mb-3 md:mb-0 md:absolute top-[-30px] right-0">
       <RoomSortItem sort={1}>料金が安い順</RoomSortItem>
       <RoomSortItem sort={-1}>料金が高い順</RoomSortItem>
-    </UL_SortList>
+    </ul>
   );
 };
 
