493005433eb8a41fa833206e26ef28f34002552b
tasukuwatanabe <tasuku.watanabe.325@gmail.com>
2024-09-02 23:53:07 +0900

    fix: WrapperをTailwind CSSで書き換え

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -1,15 +1,7 @@
-import styled from "styled-components";
-
 import { ChildrenPropsType } from "@/types";
 
-const DIV_Wrapper = styled.div`
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-`;
-
 const Wrapper = ({ children }: ChildrenPropsType) => {
-  return <DIV_Wrapper>{children}</DIV_Wrapper>;
+  return <div className="min-h-[100vh] flex flex-col">{children}</div>;
 };
 
 export default Wrapper;
