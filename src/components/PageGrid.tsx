705fb7d782d7d424e0f5a9fd73d5ef1ab820ed18
tasukuwatanabe <tasuku.watanabe.325@gmail.com>
2024-09-03 00:08:29 +0900

    refactor: PageGridをTailwind CSSで書き換え

--- src/components/PageGrid.tsx
+++ src/components/PageGrid.tsx
@@ -1,20 +1,11 @@
-import styled from "styled-components";
-
 import { ChildrenPropsType } from "@/types";
 
-const DIV_PageGrid = styled.div`
-  width: 100%;
-  display: grid;
-  grid-template-columns: 300px auto;
-  column-gap: 20px;
-  @media screen and (max-width: 767px) {
-    grid-template-columns: 1fr;
-    row-gap: 50px;
-  }
-`;
-
 const PageGrid = ({ children }: ChildrenPropsType) => {
-  return <DIV_PageGrid>{children}</DIV_PageGrid>;
+  return (
+    <div className="grid grid-cols-1 gap-y-10 md:grid-cols-pageGrid md:gap-x-10">
+      {children}
+    </div>
+  );
 };
 
 export default PageGrid;
