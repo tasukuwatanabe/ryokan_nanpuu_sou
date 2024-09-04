8fee10509d0f0002b2f5b62ffaa07fcd47f11540
tasukuwatanabe <tasuku.watanabe.325@gmail.com>
2024-09-05 00:04:36 +0900

    fix: PageGridのスタイル改善

--- src/components/PageGrid.tsx
+++ src/components/PageGrid.tsx
@@ -2,7 +2,7 @@ import { ChildrenPropsType } from "@/types";
 
 const PageGrid = ({ children }: ChildrenPropsType) => {
   return (
-    <div className="grid grid-cols-1 gap-y-10 md:grid-cols-pageGrid md:gap-x-10">
+    <div className="grid grid-cols-1 gap-y-10 md:grid-cols-pageGrid md:gap-x-5 lg:gap-x-8">
       {children}
     </div>
   );
