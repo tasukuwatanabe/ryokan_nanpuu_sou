1a22013d9e5796725854ca028a8572d6ee115b72
tasukuwatanabe <tasuku.watanabe.325@gmail.com>
2024-09-11 08:51:19 +0900

    refactor: RegisterForm内のjsxをroutesファイル側に移動

--- src/routes/register.tsx
+++ src/routes/register.tsx
@@ -1,8 +1,19 @@
 import { createFileRoute } from "@tanstack/react-router";
 
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
 import RegisterForm from "@/components/RegisterForm";
 
-const Register = () => <RegisterForm />;
+const Register = () => (
+  <Card className="w-full mx-auto max-w-md">
+    <CardHeader>
+      <CardTitle className="text-2xl">新規登録</CardTitle>
+    </CardHeader>
+    <CardContent className="grid gap-8">
+      <RegisterForm />
+    </CardContent>
+  </Card>
+);
 
 export const Route = createFileRoute("/register")({
   component: Register,
