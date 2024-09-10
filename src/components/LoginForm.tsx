271a94378fb1d9f96b9af37373cb45df8ab2fbfc
tasukuwatanabe <tasuku.watanabe.325@gmail.com>
2024-09-11 08:49:55 +0900

    refactor: LoginForm内のjsxをroutesファイル側に移動

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -10,7 +10,6 @@ import {
 import { useAuth } from "@/contexts/authContext";
 
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import {
   Form,
@@ -63,79 +62,66 @@ const LoginForm = () => {
   };
 
   return (
-    <Card className="w-full mx-auto max-w-md">
-      <CardHeader>
-        <CardTitle className="text-2xl">ログイン</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="grid gap-8">
-            <div className="grid gap-4">
-              <div className="grid gap-2">
-                <FormField
-                  control={form.control}
-                  name="email"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>メールアドレス</FormLabel>
-                      <FormControl>
-                        <Input placeholder="hello@email.com" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
-              <div className="grid gap-2">
-                <FormField
-                  control={form.control}
-                  name="password"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>パスワード</FormLabel>
-                      <FormControl>
-                        <Input {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <div className="">
-                  <Link
-                    to="/"
-                    className="ml-auto inline-block text-sm underline"
-                  >
-                    パスワードを忘れた場合
-                  </Link>
-                </div>
-              </div>
-            </div>
-            <div className="grid gap-4">
-              <Button
-                type="submit"
-                className="w-full bg-sky-500 hover:bg-sky-400"
-              >
-                ログイン
-              </Button>
-              <Button
-                type="button"
-                variant="outline"
-                onClick={handleSignInWithGoogle}
-                className="w-full"
-              >
-                Googleでログイン
-              </Button>
-            </div>
-            <div className="text-center text-sm">
-              アカウントをお持ちでない方は{" "}
-              <Link to="/register" className="underline">
-                新規登録
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="grid gap-8">
+        <div className="grid gap-4">
+          <div className="grid gap-2">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>メールアドレス</FormLabel>
+                  <FormControl>
+                    <Input placeholder="hello@email.com" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="grid gap-2">
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>パスワード</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="">
+              <Link to="/" className="ml-auto inline-block text-sm underline">
+                パスワードを忘れた場合
               </Link>
             </div>
-          </form>
-        </Form>
-      </CardContent>
-    </Card>
+          </div>
+        </div>
+        <div className="grid gap-4">
+          <Button type="submit" className="w-full bg-sky-500 hover:bg-sky-400">
+            ログイン
+          </Button>
+          <Button
+            type="button"
+            variant="outline"
+            onClick={handleSignInWithGoogle}
+            className="w-full"
+          >
+            Googleでログイン
+          </Button>
+        </div>
+        <div className="text-center text-sm">
+          アカウントをお持ちでない方は{" "}
+          <Link to="/register" className="underline">
+            新規登録
+          </Link>
+        </div>
+      </form>
+    </Form>
   );
 };
 
--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,8 +1,19 @@
 import { createFileRoute } from "@tanstack/react-router";
 
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
 import LoginForm from "@/components/LoginForm";
 
-const Login = () => <LoginForm />;
+const Login = () => (
+  <Card className="w-full mx-auto max-w-md">
+    <CardHeader>
+      <CardTitle className="text-2xl">ログイン</CardTitle>
+    </CardHeader>
+    <CardContent>
+      <LoginForm />
+    </CardContent>
+  </Card>
+);
 
 export const Route = createFileRoute("/login")({
   component: Login,
