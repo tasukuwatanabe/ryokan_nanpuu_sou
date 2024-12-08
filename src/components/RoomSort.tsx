592b20081d9b90fdf0c2a1b8566ed8655da662c6
tasukuwatanabe <tasuku.watanabe.325@gmail.com>
2024-12-08 21:21:25 +0900

    fix: Roomの料金ソート表示を修正

--- src/components/RoomSort.tsx
+++ src/components/RoomSort.tsx
@@ -29,7 +29,7 @@ const SortList = ({ sortType, setSortType }: RoomSortT) => {
   };
 
   return (
-    <div className="flex justify-end">
+    <div className="flex justify-end relative">
       <ul className="flex border-l-2 mb-3 md:mb-0 md:absolute md:top-[-30px] md:right-0">
         <RoomSortItem sort={1}>料金が安い順</RoomSortItem>
         <RoomSortItem sort={-1}>料金が高い順</RoomSortItem>
