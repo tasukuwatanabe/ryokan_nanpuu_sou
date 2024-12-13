feb3b062f32892c724c6be67ab7feb56bebc7f6a
tasukuwatanabe <tasuku.watanabe.325@gmail.com>
2024-12-13 09:09:24 +0900

    fix: Headerの重なり順を修正

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import { Link } from "@tanstack/react-router";
 
 const Header = () => {
   return (
-    <header className="sticky top-0 border-b bg-background">
+    <header className="sticky top-0 border-b bg-background z-50">
       <div className="max-w-[1000px] flex h-16 justify-between items-center m-auto px-4 md:px-6">
         <nav className="flex-col gap-6 text-lg font-medium">
           <Link to="/" className="text-xl">
